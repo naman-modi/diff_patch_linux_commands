fa658f7bb5444c2bc895c59b10084742886bf5e4
junoon-namo <namanmodi6@gmail.com>
2018-11-12 22:58:21 +0530

    Added test file

---
+++ 1.c
@@ -0,0 +1,14 @@
+hello my
+name is
+naman modi
+dad name
+is dhiren
+dhirennn
+modi
+mummma is 
+hemali modi
+she is
+amazingly
+talented
+we live
+in mumbai
---
+++ 2.c
@@ -0,0 +1,13 @@
+Hi friends
+my name is 
+naman modi
+fathers name
+is dhiren
+modi
+mom is
+hemali modi
+she is sweet
+amzingly
+talented
+we live in
+mumbai
